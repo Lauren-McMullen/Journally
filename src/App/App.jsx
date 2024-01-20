20d7d7298bcca463efbc93d1431eb3b70af197da
Lauren-McMullen <laurenmcmullen0816@gmail.com>
2024-01-20 15:50:05 -0800

    "Added beginning of css for header

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -2,20 +2,19 @@ import { FC } from 'react'
 import { useState } from "react"
 
 
-
+import logo from "../images/Quill_With_Ink.png"
 import MoodChecklist from "../components/MoodChecklist.jsx"
+import Header from "../components/Header.jsx"
 
 import './style.css'
 
 export const App = ({ name }) => {
   return (
     <div>
-      
-      <h1>Welcome to {name}!</h1>
+      <Header logo={logo} name={name}/>
+
       <p>How are you feeling today?</p>
       <MoodChecklist  sad="Sad" mad="Mad" />
-      
-
     </div>
   );
 };
