ffe63a660de05f6b5df35b7ee52f0e8e2a2372d2
mistysky04 <hediemahmoudian@gmail.com>
2024-01-20 18:27:33 -0800

    Added pages and deleted "welcome to" in header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,6 @@ export default function Header(props) {
     return (
         <div className="header">
             <img src={props.logo} alt="Quill in Ink"/>
-            <h1>Welcome to {props.name}!</h1>
         </div>
     );
 };
---
+++ src/components/PromptsPage.jsx
@@ -0,0 +1,5 @@
+import "../prompt.js";
+
+return(
+    <h1>Hello World</h1>
+)
